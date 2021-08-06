70a4e8d7979207c1436e67102ebd8936862ca57a
immortalprogrammist <immortalvelial@icloud.com>
2021-08-06 18:32:54 +0200

    -

---
+++ WeatherApp/Model/CurrentWeather.swift
@@ -0,0 +1,14 @@
+//
+//  CurrentWeather.swift
+//  WeatherApp
+//
+//  Created by Nikita on 06.08.21.
+//
+
+struct CurrentWeather {
+    let name: String
+    let country: String
+    let weather: Weather
+    let main: Main
+    let wind: Wind
+}
---
+++ WeatherApp/Model/Main.swift
@@ -0,0 +1,15 @@
+//
+//  Main.swift
+//  WeatherApp
+//
+//  Created by Nikita on 06.08.21.
+//
+
+struct Main {
+    let temp: Int
+    let feels_like: Int
+    let temp_min: Int
+    let temp_max: Int
+    let pressure: Int
+    let humidity: Int
+}
